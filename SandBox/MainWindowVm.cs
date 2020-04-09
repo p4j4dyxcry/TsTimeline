98edfb4f4e15b4c525973dfbdf6cc3ad31b9d568
yuki.tsuneyama <at12k313@gmail.com>
2020-04-09 15:26:19 +0900

    add sandbox

--- SandBox/MainWindowVm.cs
+++ SandBox/MainWindowVm.cs
@@ -1,7 +1,58 @@
-﻿namespace SandBox
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TsTimeline;
+
+namespace SandBox
 {
-    public class MainWIndowVm
+    public class TriggerClipVm : Notification, ITriggerClipDataContext
     {
-        
+        public double Value { get; set; }
+    }
+
+    public class HoldClipVm : Notification, IHoldClipDataContext
+    {
+        public double StartValue { get; set; }
+        public double EndValue { get; set; }
+    }
+
+    public class TrackVm : Notification
+    {
+        public ObservableCollection<Notification> Clips { get; } = new ObservableCollection<Notification>();
+    }
+
+    public class MainWindowVm : Notification
+    {
+        public ObservableCollection<TrackVm> Tracks { get; } = new ObservableCollection<TrackVm>();
+
+        public MainWindowVm()
+        {
+            var maxinum = 4000;
+            var rand = new Random();
+            foreach (var track in Enumerable.Range(0, 500))
+            {
+                var start = rand.Next(maxinum);
+                var end = start + rand.Next(maxinum - start);
+                Tracks.Add(new TrackVm()
+                {
+                    Clips =
+                    {
+                        new HoldClipVm()
+                        {
+                            StartValue = start,
+                            EndValue = end,
+                        },
+                        new TriggerClipVm()
+                        {
+                            Value = rand.Next(maxinum),
+                        },
+                        new TriggerClipVm()
+                        {
+                            Value = rand.Next(maxinum),
+                        }
+                    }
+                });
+            }
+        }
     }
 }
--- SandBox/Notification.cs
+++ SandBox/Notification.cs
@@ -1,7 +1,25 @@
-﻿namespace SandBox
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SandBox
 {
-    public class Notification
+    public class Notification : INotifyPropertyChanged
     {
-        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(property, value))
+                return false;
+
+            property = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
