7fb8198c18e36cb18cc36fd35f8dda40cee552fc
yuki.tsuneyama <at12k313@gmail.com>
2020-04-09 17:08:14 +0900

    fix data template selector

--- TsTimeline/InterfaceDataTemplateSelector.cs
+++ TsTimeline/InterfaceDataTemplateSelector.cs
@@ -1,31 +1,70 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace TsTimeline
 {
+    /// <summary>
+    /// interface が アサイン可能なテンプレートを選びます。
+    /// </summary>
     public class InterfaceTemplateSelector : DataTemplateSelector
     {
-        readonly string[] _resourceKeys;
+        private static DataTemplate NotFoundDataTemplate;
 
-        public InterfaceTemplateSelector(string[] resourceKeys)
+        private DataTemplate TryGetNotFoundDataTemplate()
         {
-            this._resourceKeys = resourceKeys;
-        }
+            if (NotFoundDataTemplate is null)
+            {
+                NotFoundDataTemplate = new DataTemplate();
 
+                var visualTree = new FrameworkElementFactory(typeof(TextBlock));
+                visualTree.SetBinding(TextBlock.TextProperty , new Binding());
+                
+                NotFoundDataTemplate.VisualTree = visualTree;
+            }
+
+            return NotFoundDataTemplate;
+        }
+        
+        
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
             var c = (FrameworkElement) container;
-            var dataTemplates = _resourceKeys
-                .Select(resourceKey => c.TryFindResource(resourceKey))
-                .OfType<DataTemplate>()
-                .Where(x => x.DataType is Type);
+            
+            if(item is null)
+                return TryGetNotFoundDataTemplate();
+            
+            var interfaces = item.GetType().GetInterfaces();
+            
+            // step 1 実タイプと一致するデータテンプレートを見つけた場合
+            var type = item.GetType();
+            {
+                if (c.TryFindResource(new DataTemplateKey(type)) is DataTemplate template)
+                {
+                    return template;
+                }                
+            }
 
-            var result = dataTemplates
-                .FirstOrDefault(x => ((Type) x.DataType).IsInstanceOfType(item));
+            // step 2 インターフェースに一致するテンプレートを検索する
+            foreach (var @interface in interfaces)
+            {
+                if (c.TryFindResource(new DataTemplateKey(@interface)) is DataTemplate template)
+                {
+                    return template;
+                }
+            }
 
-            return result ?? base.SelectTemplate(item, container);
+            // step 3 基底タイプを検索し一致するテンプレートを検索する
+            while (type.BaseType != null && type.BaseType != typeof(System.Object))
+            {
+                type = type.BaseType;
+                if (c.TryFindResource(new DataTemplateKey(type)) is DataTemplate template)
+                {
+                    return template;
+                }
+            }
+            
+            return TryGetNotFoundDataTemplate();
         }
     }
 }
--- TsTimeline/InterfaceTemplateSelectorExtensions.cs
+++ TsTimeline/InterfaceTemplateSelectorExtensions.cs
@@ -4,23 +4,9 @@ namespace TsTimeline
 {
     public class InterfaceTemplateSelectorExtension : System.Windows.Markup.MarkupExtension
     {
-        private string ResourceKeys { get; }
-
-        private string ResourceKeysSeparator { get; }
-
-        public InterfaceTemplateSelectorExtension(string resourceKeysCsv) : this( resourceKeysCsv , "," )
-        {
-        }
-
-        private InterfaceTemplateSelectorExtension(string resourceKeys, string separator)
-        {
-            ResourceKeys = resourceKeys;
-            ResourceKeysSeparator = separator;
-        }
-
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return new InterfaceTemplateSelector(ResourceKeys.Split(new [] {ResourceKeysSeparator}, StringSplitOptions.RemoveEmptyEntries));
+            return new InterfaceTemplateSelector();
         }
     }
 }
